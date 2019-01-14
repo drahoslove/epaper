44b703a372b783b851d8954898b4f7e3e52aaf2e
Drahoslav <drahoslav@yo2.cz>
2019-01-14 22:27:34 +0100

    Improve image.Mono test

--- image/mono_test.go
+++ image/mono_test.go
@@ -11,6 +11,8 @@ func TestMono(t *testing.T) {
 	epaper.Use(epd.Module)
 	epaper.Init("full")
 	m := NewMono(epd.Dimension.WIDTH, epd.Dimension.HEIGHT)
+	m.StrokeRect(5, 10, 55, 110)
+	m.FillRect(15, 15, 45, 100)
 
 	epaper.SetFrame(m.Bitmap(), 0, 0, m.Width(), m.Height())
 	epaper.DisplayFrame()
