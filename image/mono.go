484084aa218f893da8a4c524a5b3f8067018c41e
Drahoslav <drahoslav@yo2.cz>
2019-01-16 00:05:34 +0100

    Improve image.Mono

--- image/mono.go
+++ image/mono.go
@@ -1,7 +1,6 @@
 package image
 
 import (
-	"bytes"
 	"github.com/golang/freetype/truetype"
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/gofont/gobold"
@@ -11,14 +10,26 @@ import (
 )
 
 var (
-	goFont *truetype.Font
+	goFont     *truetype.Font
+	colorModel color.Model
 )
 
 func init() {
 	goFont, _ = truetype.Parse(gobold.TTF)
+	colorModel = color.ModelFunc(func(c color.Color) color.Color {
+		r, g, b, _ := c.RGBA()
+		avg := (r + g + b) / 3
+		if avg < 1<<15 { // tresholding
+			return color.Black
+		} else {
+			return color.White
+		}
+	})
 }
 
-// Mono implements image.Image and image/draw.Image
+// Mono is monochromatic image
+//
+// It implements image.Image and image/draw.Image interface
 type Mono []byte
 
 func NewMono(width, height uint) Mono {
@@ -28,91 +39,110 @@ func NewMono(width, height uint) Mono {
 	return bitmap
 }
 
+// Bitmap returns byte slice containing actual image data
 func (m Mono) Bitmap() []byte {
 	return m[4:]
 }
 
+// Width returns widht of image
 func (m Mono) Width() uint {
 	return uint(m[0])<<8 + uint(m[1])
 }
 
+// Height return height of image
 func (m Mono) Height() uint {
 	return uint(m[2])<<8 + uint(m[3])
 }
 
+// Set sets color on given coordinates.
+// Color should be either color.Black or color.White.
+//
+// Implements image/draw.Image interface.
 func (m Mono) Set(x, y int, c color.Color) {
 	if x < 0 || y < 0 || x >= int(m.Width()) || y >= int(m.Height()) {
 		return
 	}
 	i := uint(x) + uint(y)*m.Width()
-	r, _, _, _ := c.RGBA()
-	if r == 0 {
+	Y := colorModel.Convert(c).(color.Gray16).Y
+	if Y == 0 {
 		m[4+i/8] &^= (1 << (7 - i%8)) // clr
 	} else {
 		m[4+i/8] |= (1 << (7 - i%8)) // set
 	}
 }
 
+// At returns color at giver coordinates.
+// returned values are either color.Black or color.White.
+//
+// Implements image.Image iterface.
 func (m Mono) At(x, y int) color.Color {
 	i := uint(x) + uint(y)*m.Width()
-	r := m[4+i/8] & (1 << (7 - i%8))
-	if r == 0 {
+	bit := m[4+i/8] & (1 << (7 - i%8))
+	if bit == 0 {
 		return color.Black
 	} else {
 		return color.White
 	}
 }
 
+// Bounds returns Rectangle bounding the image.
+//
+// Implements image.Image interface.
 func (m *Mono) Bounds() image.Rectangle {
 	return image.Rect(0, 0, int(m.Width()), int(m.Height()))
 }
 
+// ColorModel return color.Model of the image.
+// Color converted to this model results either to color.Black or color.White.
+// Basic fixed tresholding method is used.
+//
+// Implement image.Image interface.
 func (m *Mono) ColorModel() color.Model {
-	return color.ModelFunc(func(c color.Color) color.Color {
-		r, g, b, _ := c.RGBA()
-		avg := (r + g + b) / 3
-		if avg > 1<<15 { // basic tresholding
-			return color.Black
-		} else {
-			return color.White
-		}
-	})
+	return colorModel
 }
 
+// Clear sets whole bitmap to given color - color.Black or color.White
 func (m *Mono) Clear(c color.Color) {
-	Y, _, _, _ := c.RGBA()
-	*m = append([]byte(*m)[:4], bytes.Repeat([]byte{byte(Y)}, len(*m)-4)...)
+	r, g, b, _ := c.RGBA()
+	Y := byte((r + g + b) / 3)
+	for i := 4; i < len(*m); i++ {
+		(*m)[i] = Y
+	}
 }
 
-func (m *Mono) DrawHorizontalLine(y, x_start, x_end int) {
+// DrawHorizontalLine draws horizontal line at given coordinates
+func (m *Mono) DrawHorizontalLine(color color.Color, y, x_start, x_end int) {
 	for x := x_start; x < x_end; x++ {
-		m.Set(x, y, color.Black)
+		m.Set(x, y, color)
 	}
 }
 
-func (m *Mono) DrawVerticalLine(x, y_start, y_end int) {
+// DrawHorizontalLine draws vettical line at given coordinates
+func (m *Mono) DrawVerticalLine(color color.Color, x, y_start, y_end int) {
 	for y := y_start; y < y_end; y++ {
-		m.Set(x, y, color.Black)
+		m.Set(x, y, color)
 	}
 }
 
-func (m *Mono) StrokeRect(x_start, y_start, x_end, y_end int) {
-	m.DrawHorizontalLine(y_start, x_start, x_end)
-	m.DrawHorizontalLine(y_end, x_start, x_end)
-	m.DrawVerticalLine(x_start, y_start, y_end)
-	m.DrawVerticalLine(x_end, y_start, y_end)
+// StrokeRect draws outline of rectangle given coord of two oposite corners
+func (m *Mono) StrokeRect(color color.Color, x_start, y_start, x_end, y_end int) {
+	m.DrawHorizontalLine(color, y_start, x_start, x_end)
+	m.DrawHorizontalLine(color, y_end, x_start, x_end)
+	m.DrawVerticalLine(color, x_start, y_start, y_end)
+	m.DrawVerticalLine(color, x_end, y_start, y_end)
 }
 
-func (m *Mono) FillRect(x_start, y_start, x_end, y_end int) {
+// StrokeRect draws filled rectangle given coord of two oposite corners
+func (m *Mono) FillRect(color color.Color, x_start, y_start, x_end, y_end int) {
 	for y := y_start; y < y_end; y++ {
-		m.DrawHorizontalLine(y, x_start, x_end)
+		m.DrawHorizontalLine(color, y, x_start, x_end)
 	}
 }
 
-func (m *Mono) DrawString(text string, size float64, x, y int) {
+func (m *Mono) DrawString(color color.Color, text string, size float64, x, y int) {
 	d := font.Drawer{
 		Dst: m,
-		Src: image.Black,
+		Src: image.NewUniform(color),
 		Face: truetype.NewFace(goFont, &truetype.Options{
 			Size:    size,
 			Hinting: font.HintingFull,
@@ -122,13 +152,16 @@ func (m *Mono) DrawString(text string, size float64, x, y int) {
 	d.DrawString(text)
 }
 
-func flip(b byte) byte {
+// flips order of bits in byte
+func flipByte(b byte) byte {
 	b = (b&0xF0)>>4 | (b&0x0F)<<4
 	b = (b&0xCC)>>2 | (b&0x33)<<2
 	b = (b&0xAA)>>1 | (b&0x55)<<1
 	return b
 }
 
+// VerticalFlip flips image vertically (along horizontal axe)
+// top to bottom and bottom to top
 func (m *Mono) VerticalFlip() {
 	data := m.Bitmap()
 	w := m.Width() / 8
@@ -140,17 +173,22 @@ func (m *Mono) VerticalFlip() {
 	}
 }
 
+// HorizontalFlip flips image horizontally (along vetical axe)
+// left to right and right to left
 func (m *Mono) HorizontalFlip() {
 	data := m.Bitmap()
 	w := m.Width() / 8
 	h := m.Height()
 	for y := uint(0); y < h; y++ {
 		for i := uint(0); i < w/2; i++ {
-			data[y*w+i], data[y*w+w-1-i] = flip(data[y*w+w-1-i]), flip(data[y*w+i])
+			data[y*w+i], data[y*w+w-1-i] = flipByte(data[y*w+w-1-i]), flipByte(data[y*w+i])
 		}
 	}
 }
 
+// RotateRight will rotate image 90 degrees clockwise.
+//
+// (center of rotation is center of largest square fitted to top left)
 func (m *Mono) RotateRight() {
 	w := m.Width()
 	h := m.Height()
@@ -165,6 +203,9 @@ func (m *Mono) RotateRight() {
 	*m = n
 }
 
+// RotateLeft will rotate image 90 degrees counterclockwise.
+//
+// (center of rotation is center of largest square fitted to top left)
 func (m *Mono) RotateLeft() {
 	w := m.Width()
 	h := m.Height()
@@ -177,4 +218,11 @@ func (m *Mono) RotateLeft() {
 		}
 	}
 	*m = n
+}
+
+// Invert inverts colors in image
+func (m *Mono) Invert() {
+	for i := 4; i < len(*m); i++ {
+		(*m)[i] = ^(*m)[i]
+	}
 }
--- image/mono_test.go
+++ image/mono_test.go
@@ -9,21 +9,23 @@ import (
 )
 
 func TestMono(t *testing.T) {
+	black := color.Black
+	white := color.White
 
 	epaper.Use(epd.Module)
 	epaper.Init("full")
 	m := NewMono(epd.Dimension.HEIGHT, epd.Dimension.WIDTH)
-	m.Clear(color.White)
-	m.Set(1, 1, color.Black)
-	m.Set(1, 2, color.White)
-	m.Set(1, 3, color.Black)
-	m.Set(1, 4, color.Black)
-	m.StrokeRect(5, 5, 45, 65)
-	m.FillRect(10, 10, 40, 60)
+	m.Clear(white)
+	m.Set(1, 1, black)
+	m.Set(1, 2, white)
+	m.Set(1, 3, black)
+	m.Set(1, 4, black)
+	m.StrokeRect(black, 5, 5, 45, 65)
+	m.FillRect(black, 10, 10, 40, 60)
 
 	for i, s := range []string{"Hello World", "12:59 | 23.5°C"} {
 		size := 38
-		m.DrawString(s, float64(size), 50, size*(i+1))
+		m.DrawString(black, s, float64(size), 50, size*(i+1))
 	}
 
 	// m.HorizontalFlip()
@@ -31,10 +33,13 @@ func TestMono(t *testing.T) {
 	m.RotateRight()
 
 	for i, n := 0, 2; i < 10; i, n = i+1, n+n/2 {
-		m.DrawHorizontalLine(n+50, 3*i, 50-3*i)
+		m.DrawHorizontalLine(black, n+50, 3*i, 50-3*i)
 	}
 
-	m.DrawString("abc", 24, 5, 30)
+	m.DrawString(black, "abc", 24, 5, 30)
+	m.DrawString(white, "abc", 24, 70, 30)
+
+	m.Invert()
 
 	epaper.SetFrame(m.Bitmap(), 0, 0, m.Width(), m.Height())
 	epaper.DisplayFrame()
