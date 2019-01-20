a3b78a75942c5362b558713db79957f827858a4e
Drahoslav <drahoslav@yo2.cz>
2019-01-20 04:54:10 +0100

    Draw minigraph in thermostat example

--- example/thermostat/thermostat.go
+++ example/thermostat/thermostat.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/hex"
-	"fmt"
+	// "fmt"
 	"image"
 	"image/color"
 	"io/ioutil"
@@ -15,11 +15,14 @@ import (
 	eimage "github.com/drahoslove/epaper/image"
 )
 
-const zeroK = -273.15
+const zeroK = float32(-273.15)
+
+var (
+	tempIndex = 0
+)
 
 func main() {
-	temps := [][4]float32{}
-	tempIndex := 0
+	temps := [][6]float32{}
 
 	devices, err := ioutil.ReadDir("/sys/bus/w1/devices/")
 	if err != nil {
@@ -34,7 +37,7 @@ func main() {
 			shortName := name[5:7] + "/" + name[13:15]
 			names = append(names, name)
 			shortNames = append(shortNames, shortName)
-			temps = append(temps, [4]float32{zeroK, zeroK, zeroK, zeroK})
+			temps = append(temps, [6]float32{zeroK, zeroK, zeroK, zeroK, zeroK, zeroK})
 		}
 	}
 
@@ -57,7 +60,7 @@ func main() {
 			temps[i][tempIndex] = temp
 		}
 		tempIndex++
-		tempIndex %= 4
+		tempIndex %= 6
 	}
 }
 
@@ -76,8 +79,8 @@ func readTemp(devName string) float32 {
 	return temp
 }
 
-func render(names []string, temps [][4]float32, t time.Time) {
-	if t.Second() == 0 {
+func render(names []string, temps [][6]float32, t time.Time) {
+	if t.Second() == 0 && t.Minute()%2 == 1 {
 		epaper.Init("full")
 		defer epaper.Init("partial")
 	}
@@ -87,17 +90,39 @@ func render(names []string, temps [][4]float32, t time.Time) {
 	img.FillRect(image.Black, irect)
 
 	for i, name := range names {
-		avgTemp := float64((temps[i][0] + temps[i][1] + temps[i][2] + temps[i][3]) / 4)
+		avgTemp := float64((temps[i][0] + temps[i][1] + temps[i][2] + temps[i][3] + temps[i][4] + temps[i][5]) / 6)
 		tempStr := strconv.FormatFloat(avgTemp, 'f', 2, 32)
 
-		fmt.Println(name, tempStr)
+		// fmt.Println(name, tempStr)
 
-		img.DrawString(color.White, name+":", 26, image.Pt(5, 30*(i+1)))
-		img.DrawString(color.White, tempStr+"°C", 26, image.Pt(105, 30*(i+1)))
+		img.DrawString(color.White, name+":", 20, image.Pt(5, 30*(i+1)))
+		img.DrawString(color.White, tempStr+"°C", 26, image.Pt(85, 30*(i+1)))
+		renderProgress(img, color.White, image.Pt(200, 30*(i+1)-15), temps[i])
 	}
 
 	img.RotateRight()
 	img.DrawString(color.White, t.String()[11:19], 28, image.Pt(5, 280))
 	epaper.Display(img.Bitmap(), 0, 0, img.Width(), img.Height())
 	// epaper.Sleep()
+}
+
+func renderProgress(img eimage.Mono, color color.Color, pos image.Point, temps [6]float32) {
+	max := zeroK
+	for _, temp := range temps {
+		if temp > max {
+			max = temp
+		}
+	}
+	ys := [6]int{}
+	for i, n := tempIndex, 0; n < 6; i, n = (i+1)%6, n+1 {
+		ys[n] = int((max - temps[i]) * 10)
+	}
+	for n := 2; n < 6; n++ {
+		img.DrawLine(
+			color,
+			pos.Add(image.Pt(0, ys[n-1])),
+			pos.Add(image.Pt(10, ys[n])),
+		)
+		pos = pos.Add(image.Pt(10, 0))
+	}
 }
